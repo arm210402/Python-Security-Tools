597ca597719dd69a4f91ff6e045e137e3661c0a0
arm210402 <59504320+arm210402@users.noreply.github.com>
2023-05-06 23:43:38 +0530

    Create wifi_devices_connected.py

---
+++ wifi_devices_connected.py
@@ -0,0 +1,22 @@
+from scapy.all import *
+
+def get_mac_address(ip):
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return answered_list[0][1].hwsrc
+
+def scan_network(ip):
+    network = ip + "/24"
+    arp_request = ARP(pdst=network)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+print(scan_network('xxx.xxx.xx.x'))
